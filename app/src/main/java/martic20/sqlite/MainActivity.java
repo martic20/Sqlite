98a6e8eeaaf7b480202112b2ad048303c674db87
Martí <martic20@gmail.com>
2018-01-15 19:59:28 +0100

    bar button

--- app/src/main/java/martic20/sqlite/MainActivity.java
+++ app/src/main/java/martic20/sqlite/MainActivity.java
@@ -4,6 +4,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -43,6 +45,23 @@ public class MainActivity extends AppCompatActivity {
         AdapterVino adapter = new AdapterVino(this, vinosArray);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.mybutton) {
+            // do something here
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
